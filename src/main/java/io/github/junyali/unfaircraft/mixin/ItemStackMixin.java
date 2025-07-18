da5089f8e038f2bbb428b742a7d5e219c1aa665c
Junya <junjia.liu@pm.me>
2025-07-19 05:08:47 +0800

    Make random item durability loss more varied

--- src/main/java/io/github/junyali/unfaircraft/mixin/ItemStackMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/ItemStackMixin.java
@@ -12,13 +12,22 @@ public class ItemStackMixin {
 	@Unique
 	private static final float DURABILITY_LOSS_CHANCE = 0.05f;
 
+	@Unique
+	private static final int DURABILITY_DAMAGE_MIN = 2;
+
+	@Unique
+	private static final int DURABILITY_DAMAGE_MAX = 16;
+
 	@ModifyVariable(method = "hurtAndBreak(ILnet/minecraft/server/level/ServerLevel;Lnet/minecraft/server/level/ServerPlayer;Ljava/util/function/Consumer;)V", at = @At("HEAD"), argsOnly = true, ordinal = 0)
 	private int modifyDurabilityDamage(int amount) {
 		ItemStack stack = (ItemStack) (Object) this;
 
 		if (stack.getItem() instanceof TieredItem) {
 			if (Math.random() < DURABILITY_LOSS_CHANCE) {
-				return amount + 2;
+				// more maths owo
+				int durabilityLoss = DURABILITY_DAMAGE_MIN + (int) (Math.random() * (DURABILITY_DAMAGE_MAX - DURABILITY_DAMAGE_MIN + 1));
+
+				return amount + durabilityLoss;
 			}
 		}
 		return amount;
