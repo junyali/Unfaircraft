be3ae296808037aa4efab00db69087f095d2c8f9
Junya <junjia.liu@pm.me>
2025-07-18 18:31:31 +0800

    Remove unnecessary imports

--- src/main/java/io/github/junyali/unfaircraft/mixin/PlayerMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/PlayerMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.world.item.SwordItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Player.class)
