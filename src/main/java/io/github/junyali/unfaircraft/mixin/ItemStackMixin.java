23935beaa0f537207e0611b899bc35b1f286a188
Junya <junjia.liu@pm.me>
2025-07-18 19:38:55 +0800

    Remove unnecessary imports

--- src/main/java/io/github/junyali/unfaircraft/mixin/ItemStackMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/ItemStackMixin.java
@@ -1,6 +1,5 @@
 package io.github.junyali.unfaircraft.mixin;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TieredItem;
 import org.spongepowered.asm.mixin.Mixin;
