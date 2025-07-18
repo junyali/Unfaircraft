1cbad3e95d5797f203a388b55ee7d7fedccc014e
Junya <junjia.liu@pm.me>
2025-07-19 05:42:55 +0800

    Make combat even more deadlier for oneself :p

--- src/main/java/io/github/junyali/unfaircraft/mixin/PlayerMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/PlayerMixin.java
@@ -1,5 +1,7 @@
 package io.github.junyali.unfaircraft.mixin;
 
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,6 +14,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class PlayerMixin {
 	@Unique
 	private static final float CRIT_FAIL_CHANCE = 0.25f;
+
+	@Unique
+	private static final float SELF_ATTACK_CHANCE = 0.05f;
+
+	@Unique
+	private static final float EXHAUSTION_CHANCE = 0.2f;
 
 	@Inject(method = "attack", at = @At("HEAD"), cancellable = true)
 	private void beforeAttack(Entity target, CallbackInfo ci) {
@@ -28,6 +36,16 @@ public class PlayerMixin {
 			if (player.level().random.nextFloat() < CRIT_FAIL_CHANCE) {
 				ci.cancel();
 			}
+		}
+
+		if (player.level().random.nextFloat() < SELF_ATTACK_CHANCE) {
+			float damage = 1.0f + player.level().random.nextFloat() * 2.0f;
+			player.hurt(player.level().damageSources().generic(), damage);
+			player.level().playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_HURT, SoundSource.PLAYERS, 1.0f, 1.0f);
+		}
+
+		if (player.level().random.nextFloat() < EXHAUSTION_CHANCE) {
+			player.causeFoodExhaustion(4.0f);
 		}
 	}
 }
