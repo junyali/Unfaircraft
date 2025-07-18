62d2b92011a653fc22edc068f8fa6ed6c5f34863
Junya <junjia.liu@pm.me>
2025-07-18 19:19:05 +0800

    Fix ItemStack modification method

--- src/main/java/io/github/junyali/unfaircraft/mixin/ItemStackMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/ItemStackMixin.java
@@ -4,22 +4,24 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TieredItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-	@ModifyArg(method = "hurtAndBreak*", at = @At("HEAD"), index = 0)
-	private int modifyDurabilityDamage(int damage, LivingEntity entity) {
+	@Unique
+	private static final float DURABILITY_LOSS_CHANCE = 0.05f;
+
+	@ModifyVariable(method = "hurtAndBreak(ILnet/minecraft/server/level/ServerLevel;Lnet/minecraft/server/level/ServerPlayer;Ljava/util/function/Consumer;)V", at = @At("HEAD"), argsOnly = true, ordinal = 0)
+	private int modifyDurabilityDamage(int amount) {
 		ItemStack stack = (ItemStack) (Object) this;
 
-		if (stack.getItem() instanceof TieredItem && entity != null) {
-			if (entity.level().random.nextFloat() < 0.05f) {
-				return damage + 2;
+		if (stack.getItem() instanceof TieredItem) {
+			if (Math.random() < DURABILITY_LOSS_CHANCE) {
+				return amount + 2;
 			}
 		}
-
-		return damage;
+		return amount;
 	}
 }
