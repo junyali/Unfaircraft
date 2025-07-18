98bac24b66326360de201a423777aa325ac9b54c
Junya <junjia.liu@pm.me>
2025-07-19 07:00:35 +0800

    Write mixin toggles in UnfairCraftConfig

--- src/main/java/io/github/junyali/unfaircraft/config/UnfairCraftConfig.java
+++ src/main/java/io/github/junyali/unfaircraft/config/UnfairCraftConfig.java
@@ -5,5 +5,29 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class UnfairCraftConfig {
 	private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
+	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_UNFAIR_MODE = BUILDER
+			.comment("Master toggle for UnfairCraft")
+			.define("general.enable_unfair_mode", true);
+
+	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_BED_BLOCK_MIXIN = BUILDER
+			.comment("Enable Bed Block mixin")
+			.define("bed_block_mixin", true);
+
+	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_SHIELD_ITEM_MIXIN = BUILDER
+			.comment("Enable Shield Item mixin")
+			.define("shield_item_mixin", true);
+
+	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_PLAYER_MIXIN = BUILDER
+			.comment("Enable Player mixin")
+			.define("player_mixin", true);
+
+	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_ITEM_STACK_MIXIN = BUILDER
+			.comment("Enable Item Stack mixin")
+			.define("item_stack_mixin", true);
+
+	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_BOW_ITEM_MIXIN = BUILDER
+			.comment("Enable Bow Item mixin")
+			.define("bow_item_mixin", true);
+
 	public static final ModConfigSpec SPEC = BUILDER.build();
 }
