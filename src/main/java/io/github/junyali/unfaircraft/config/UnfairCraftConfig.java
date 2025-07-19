70a75e264d964ca9a51e8bb235dc52df6da5da56
Junya <junjia.liu@pm.me>
2025-07-20 07:44:45 +0800

    Cleanup config class (oml i'm so stupid)

--- src/main/java/io/github/junyali/unfaircraft/config/UnfairCraftConfig.java
+++ src/main/java/io/github/junyali/unfaircraft/config/UnfairCraftConfig.java
@@ -13,156 +13,156 @@ public class UnfairCraftConfig {
 	// individual mixin toggles
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_BED_BLOCK_MIXIN = BUILDER
 			.comment("Enable Bed Block mixin")
-			.define("bed_block_mixin", true);
+			.define("mixins.bed_block_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_SHIELD_ITEM_MIXIN = BUILDER
 			.comment("Enable Shield Item mixin")
-			.define("shield_item_mixin", true);
+			.define("mixins.shield_item_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_PLAYER_MIXIN = BUILDER
 			.comment("Enable Player mixin")
-			.define("player_mixin", true);
+			.define("mixins.player_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_ITEM_STACK_MIXIN = BUILDER
 			.comment("Enable Item Stack mixin")
-			.define("item_stack_mixin", true);
+			.define("mixins.item_stack_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_BOW_ITEM_MIXIN = BUILDER
 			.comment("Enable Bow Item mixin")
-			.define("bow_item_mixin", true);
+			.define("mixins.bow_item_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_CHEST_BLOCK_MIXIN = BUILDER
 			.comment("Enable Chest Block mixin")
-			.define("chest_block_mixin", true);
+			.define("mixins.chest_block_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_ANVIL_MENU_MIXIN = BUILDER
 			.comment("Enable Anvil Menu mixin")
-			.define("anvil_menu_mixin", true);
+			.define("mixins.anvil_menu_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_MINECART_MIXIN = BUILDER
 			.comment("Enable Minecart Mixin")
-			.define("minecat_mixin", true);
+			.define("mixins.minecat_mixin", true);
 
 	public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_FOOD_ITEM_MIXIN = BUILDER
 			.comment("Enable Food Item mixin")
-			.define("food_item_mixin", true);
+			.define("mixins.food_item_mixin", true);
 
 	// oh boy here we go...
 	public static final ModConfigSpec.ConfigValue<Double> BED_EXPLOSION_CHANCE = BUILDER
-			.comment("Chance for beds to explode on interaction (default: 0.25; range: 0.0 to 1.0)")
-			.defineInRange("bed_explosion_chance", 0.25, 0.0, 1.0);
+			.comment("Chance for beds to explode on interaction")
+			.defineInRange("bed_block_mixin.bed_explosion_chance", 0.25, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> BED_EXPLOSION_RADIUS = BUILDER
-			.comment("Radius for bed explosion (default: 5.0)")
-			.define("bed_explosion_radius", 5.0);
+			.comment("Radius for bed explosion")
+			.define("bed_block_mixin.bed_explosion_radius", 5.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> BED_FIRE_CHANCE = BUILDER
-			.comment("Chance for beds to set player on fire after interaction resulting in explosion (default: 0.5; range: 0.0 to 1.0)")
-			.defineInRange("bed_fire_chance", 0.5, 0.0, 1.0);
+			.comment("Chance for beds to set player on fire after interaction resulting in explosion")
+			.defineInRange("bed_block_mixin.bed_fire_chance", 0.5, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Integer> BED_FIRE_DURATION = BUILDER
-			.comment("Duration of bed fire in ticks (default: 200)")
-			.define("bed_fire_duration", 200);
+			.comment("Duration of bed fire in ticks")
+			.define("bed_block_mixin.bed_fire_duration", 200);
 
 	public static final ModConfigSpec.ConfigValue<Double> SHIELD_FAIL_CHANCE = BUILDER
-			.comment("Chance for shields to fail to block attacks (default: 0.10; range: 0.0 to 1.0)")
-			.defineInRange("shield_fail_chance", 0.10, 0.0, 1.0);
+			.comment("Chance for shields to fail to block attacks")
+			.defineInRange("shield_item_mixin.shield_fail_chance", 0.10, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> PLAYER_CRIT_FAIL_CHANCE = BUILDER
-			.comment("Chance for player crit attacks to fail (default: 0.25; range: 0.0 to 1.0)")
-			.defineInRange("player_crit_fail_chance", 0.25, 0.0, 1.);
+			.comment("Chance for player crit attacks to fail")
+			.defineInRange("player_mixin.player_crit_fail_chance", 0.25, 0.0, 1.);
 
 	public static final ModConfigSpec.ConfigValue<Double> PLAYER_SELF_ATTACK_CHANCE = BUILDER
-			.comment("Chance for player attacks to inflict upon themself (default: 0.05; range: 0.0 to 1.0)")
-			.defineInRange("player_self_attack_chance", 0.05, 0.0, 1.0);
+			.comment("Chance for player attacks to inflict upon themself")
+			.defineInRange("player_mixin.player_self_attack_chance", 0.05, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> PLAYER_ATTACK_EXHAUSTION_CHANCE = BUILDER
-			.comment("Chance for player attacks to cause exhaustion upon themself (default: 0.2; range: 0.0 to 1.0)")
-			.defineInRange("player_attack_exhaustion_chance", 0.2, 0.0, 1.0);
+			.comment("Chance for player attacks to cause exhaustion upon themself")
+			.defineInRange("player_mixin.player_attack_exhaustion_chance", 0.2, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> PLAYER_RANDOM_DROP_CHANCE = BUILDER
-			.comment("Chance per tick for a player to randomly drop their held item (default: 0.0000005; range: 0.0 to 1.0)")
-			.defineInRange("player_random_drop_chance", 0.0000005, 0.0, 1.0);
+			.comment("Chance per tick for a player to randomly drop their held item")
+			.defineInRange("player_mixin.player_random_drop_chance", 0.0000005, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> ITEM_DURABILITY_LOSS_CHANCE = BUILDER
-			.comment("Chance for tiered items to lose extra durability (default: 0.05; range: 0.0 to 1.0)")
-			.defineInRange("item_durability_loss_chance", 0.05, 0.0, 1.0);
+			.comment("Chance for tiered items to lose extra durability")
+			.defineInRange("item_stack_mixin.item_durability_loss_chance", 0.05, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Integer> ITEM_DURABILITY_DAMAGE_MIN = BUILDER
-			.comment("Minimum additional durability damage on items (default: 2)")
-			.define("item_durability_damage_min", 2);
+			.comment("Minimum additional durability damage on items")
+			.define("item_stack_mixin.item_durability_damage_min", 2);
 
 	public static final ModConfigSpec.ConfigValue<Integer> ITEM_DURABILITY_DAMAGE_MAX = BUILDER
-			.comment("Maximum additional durability damage on items (default: 20)")
-			.define("item_durability_damage_max", 20);
+			.comment("Maximum additional durability damage on items")
+			.define("item_stack_mixin.item_durability_damage_max", 20);
 
 	public static final ModConfigSpec.ConfigValue<Double> BOW_WONKY_CHANCE = BUILDER
-			.comment("Chance for player arrows fired from bows to deviate in direction (default: 0.25; range: 0.0 to 1.0)")
-			.defineInRange("bow_wonky_chance", 0.25, 0.0, 1.0);
+			.comment("Chance for player arrows fired from bows to deviate in direction")
+			.defineInRange("bow_item_mixin.bow_wonky_chance", 0.25, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> BOW_PROJECTILE_DEVIATION = BUILDER
-			.comment("How much player-fired arrows can deviate from their intended path (default: 0.8)")
-			.define("bow_projectile_deviation", 0.8);
+			.comment("How much player-fired arrows can deviate from their intended path")
+			.define("bow_item_mixin.bow_projectile_deviation", 0.8);
 
 	public static final ModConfigSpec.ConfigValue<Double> BOW_MISFIRE_CHANCE = BUILDER
-			.comment("Chance for player-fired bows to not shoot (default: 0.5; range: 0.0 to 1.0)")
-			.defineInRange("bow_misfire_chance", 0.5, 0.0, 1.0);
+			.comment("Chance for player-fired bows to not shoot")
+			.defineInRange("bow_item_mixin.bow_misfire_chance", 0.5, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> BOW_BACKFIRE_CHANCE = BUILDER
-			.comment("Chance for player-fired bows to damage themself when misfiring (default: 0.4; range: 0.0 to 1.0)")
-			.defineInRange("bow_backfire_chance", 0.4, 0.0, 1.0);
+			.comment("Chance for player-fired bows to damage themself when misfiring")
+			.defineInRange("bow_item_mixin.bow_backfire_chance", 0.4, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> BOW_BACKFIRE_DAMAGE_MIN = BUILDER
-			.comment("Minimum damage from bow backfire (default: 2.0")
-			.define("bow_backfire_damage_min", 2.0);
+			.comment("Minimum damage from bow backfire")
+			.define("bow_item_mixin.bow_backfire_damage_min", 2.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> BOW_BACKFIRE_DAMAGE_MAX = BUILDER
-			.comment("Maximum damage from bow backfire (default: 20.0")
-			.define("bow_backfire_damage_max", 20.0);
+			.comment("Maximum damage from bow backfire")
+			.define("bow_item_mixin.bow_backfire_damage_max", 20.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> CHEST_EAT_CHANCE = BUILDER
-			.comment("Chance for a chest to eat items when opened (default: 0.15; range: 0.0 to 1.0)")
-			.defineInRange("chest_eat_chance", 0.15, 0.0, 1.0);
+			.comment("Chance for a chest to eat items when opened")
+			.defineInRange("chest_block_entity_mixin.chest_eat_chance", 0.15, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Integer> CHEST_EAT_ITEM_MIN = BUILDER
-			.comment("Minimum number of items a chest will eat at once (default: 1)")
-			.define("chest_eat_item_min", 1);
+			.comment("Minimum number of items a chest will eat at once")
+			.define("chest_block_entity_mixin.chest_eat_item_min", 1);
 
 	public static final ModConfigSpec.ConfigValue<Integer> CHEST_EAT_ITEM_MAX = BUILDER
-			.comment("Maximum number of items a chest will eat at once (default: 32)")
-			.define("chest_eat_item_max", 32);
+			.comment("Maximum number of items a chest will eat at once")
+			.define("chest_block_entity_mixin.chest_eat_item_max", 32);
 
 	public static final ModConfigSpec.ConfigValue<Double> ANVIL_INSTANT_BREAK_CHANCE = BUILDER
-			.comment("Chance for anvils to break completely in one use (default: 0.1; range: 0.0 to 1.0)")
-			.defineInRange("anvil_instance_break_chance", 0.1, 0.0, 1.0);
+			.comment("Chance for anvils to break completely in one use")
+			.defineInRange("anvil_menu_mixin.anvil_instance_break_chance", 0.1, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> ANVIL_COST_INCREASE_CHANCE = BUILDER
-			.comment("Chance for anvil repair costs to be multiplied (default: 0.5; range: 0.0 to 1.0)")
-			.defineInRange("anvil_cost_increase_chance", 0.5, 0.0, 1.0);
+			.comment("Chance for anvil repair costs to be multiplied")
+			.defineInRange("anvil_menu_mixin.anvil_cost_increase_chance", 0.5, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> ANVIL_COST_MULTIPLIER_MIN = BUILDER
-			.comment("Minimum multiplier for increased anvil repair costs (default: 2.0)")
-			.define("anvil_cost_multiplier_min", 2.0);
+			.comment("Minimum multiplier for increased anvil repair costs")
+			.define("anvil_menu_mixin.anvil_cost_multiplier_min", 2.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> ANVIL_COST_MULTIPLIER_MAX = BUILDER
-			.comment("Maximum multiplier for increased anvil repair costs (default: 6.0)")
-			.define("anvil_cost_multiplier_max", 6.0);
+			.comment("Maximum multiplier for increased anvil repair costs")
+			.define("anvil_menu_mixin.anvil_cost_multiplier_max", 6.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> MINECART_SLOWDOWN_FACTOR = BUILDER
-			.comment("Factor to reduce minecart speed, lower = slow (default: 0.5; range: 0.0 to 1.0)")
-			.defineInRange("minecart_slowdown_factor", 0.5, 0.0, 1.0);
+			.comment("Factor to reduce minecart speed, lower = slow")
+			.defineInRange("minecart_mixin.minecart_slowdown_factor", 0.5, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> MINECART_STOP_CHANCE = BUILDER
-			.comment("Chance for minecarts to randomly stop each tick (default: 0.05; range: 0.0 to 1.0)")
-			.defineInRange("minecart_stop_chance", 0.05, 0.0, 1.0);
+			.comment("Chance for minecarts to randomly stop each tick")
+			.defineInRange("minecart_mixin.minecart_stop_chance", 0.05, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> MINECART_REVERSE_CHANCE = BUILDER
-			.comment("Chance for minecarts to randomly reverse direction each tick (default: 0.01; range: 0.0 to 1.0)")
-			.defineInRange("minecart_reverse_chance", 0.01, 0.0, 1.0);
+			.comment("Chance for minecarts to randomly reverse direction each tick")
+			.defineInRange("minecart_mixin.minecart_reverse_chance", 0.01, 0.0, 1.0);
 
 	public static final ModConfigSpec.ConfigValue<Double> FOOD_FAIL_CHANCE = BUILDER
-			.comment("Chance for food to completely fail to restore hunger (default: 0.05; range: 0.0 to 1.0)")
-			.defineInRange("food_fail_chance", 0.05, 0.0, 1.0);
+			.comment("Chance for food to completely fail to restore hunger")
+			.defineInRange("food_item_mixin.food_fail_chance", 0.05, 0.0, 1.0);
 
 	public static final ModConfigSpec SPEC = BUILDER.build();
 }
