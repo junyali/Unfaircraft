bdc1d3a935c61e99debddc4b95c1f82e0bb1ac0d
Junya <junjia.liu@pm.me>
2025-07-21 16:21:23 +0800

    Remove unnecessary imports

--- src/main/java/io/github/junyali/unfaircraft/mixin/NightmareEventMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/NightmareEventMixin.java
@@ -24,7 +24,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BooleanSupplier;
 
