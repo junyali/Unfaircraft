5606917d7c347a8dde22443aab3bab84d814dbce
mochi <git@nyamochi.moe>
2026-04-23 00:43:29 +0100

    Setup datagen class

---
+++ src/main/java/io/github/junyali/unfaircraft/datagen/DataGenerators.java
@@ -0,0 +1,19 @@
+package io.github.junyali.unfaircraft.datagen;
+
+import io.github.junyali.unfaircraft.UnfairCraft;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+@EventBusSubscriber(modid = UnfairCraft.MODID)
+public class DataGenerators {
+	@SubscribeEvent
+	public static void gatherData(GatherDataEvent event) {
+		DataGenerator generator = event.getGenerator();
+		PackOutput packOutput = generator.getPackOutput();
+
+		// Client
+	}
+}
