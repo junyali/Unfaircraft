f6eab6307eb450214bcbae92cd661b668a799992
mochi <git@nyamochi.moe>
2026-04-22 20:33:32 +0100

    Painstakingly declare config variables

--- src/main/java/io/github/junyali/unfaircraft/config/UnfairCraftConfig.java
+++ src/main/java/io/github/junyali/unfaircraft/config/UnfairCraftConfig.java
@@ -41,127 +41,202 @@ public class UnfairCraftConfig {
 	public static final NightmareEvent NIGHTMARE_EVENT;
 
 	public static class Bed {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> explosionChance;
+		public final ModConfigSpec.ConfigValue<Double> explosionRadius;
+		public final ModConfigSpec.ConfigValue<Double> fireChance;
+		public final ModConfigSpec.ConfigValue<Integer> fireDuration;
 	}
 
 	public static class Shield {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> failChance;
 	}
 
 	public static class Player {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> critFailChance;
+		public final ModConfigSpec.ConfigValue<Double> selfAttackChance;
+		public final ModConfigSpec.ConfigValue<Double> attackExhaustionChance;
+		public final ModConfigSpec.ConfigValue<Double> randomDropChance;
+		public final ModConfigSpec.ConfigValue<Double> fallDamageDistance;
+		public final ModConfigSpec.ConfigValue<Double> fallDamageMultiplier;
+		public final ModConfigSpec.ConfigValue<Double> pickupFailChance;
 	}
 
 	public static class ItemDurability {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> lossChance;
+		public final ModConfigSpec.ConfigValue<Integer> damageMin;
+		public final ModConfigSpec.ConfigValue<Integer> damageMax;
 	}
 
 	public static class Bow {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> wonkyChance;
+		public final ModConfigSpec.ConfigValue<Double> projectileDeviation;
+		public final ModConfigSpec.ConfigValue<Double> misfireChance;
+		public final ModConfigSpec.ConfigValue<Double> backfireChance;
+		public final ModConfigSpec.ConfigValue<Double> backfireDamageMin;
+		public final ModConfigSpec.ConfigValue<Double> backfireDamaeMax;
 	}
 
 	public static class Chest {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> eatChance;
+		public final ModConfigSpec.ConfigValue<Integer> eatItemMin;
+		public final ModConfigSpec.ConfigValue<Integer> eatItemMax;
 	}
 
 	public static class Anvil {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> instantBreakChance;
+		public final ModConfigSpec.ConfigValue<Double> costIncreaseChance;
+		public final ModConfigSpec.ConfigValue<Double> costMultiplierMin;
+		public final ModConfigSpec.ConfigValue<Double> costMultiplierMax;
 	}
 
 	public static class Minecart {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> slowdownFactor;
+		public final ModConfigSpec.ConfigValue<Double> stopChance;
+		public final ModConfigSpec.ConfigValue<Double> reverseChance;
 	}
 
 	public static class Food {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> failChance;
+		public final ModConfigSpec.ConfigValue<Double> debuffChance;
 	}
 
 	public static class CaveCarver {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> extraLavaPocketChance;
 	}
 
 	public static class Ore {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> defaultReplacementChance;
+		public final ModConfigSpec.ConfigValue<Double> goldReplacementChance;
+		public final ModConfigSpec.ConfigValue<Double> emeraldReplacementChance;
+		public final ModConfigSpec.ConfigValue<Double> diamondReplacementChance;
+		public final ModConfigSpec.ConfigValue<Double> ancientDebrisReplacementChance;
 	}
 
 	public static class LootTable {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> trollChance;
 	}
 
 	public static class Sapling {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> deathChance;
 	}
 
 	public static class Farmland {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> revertChance;
 	}
 
 	public static class MobDetection {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> rangeMultiplier;
 	}
 
 	public static class BlockInteraction {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> placeFailChance;
+		public final ModConfigSpec.ConfigValue<Double> breakFailChance;
 	}
 
 	public static class Bucket {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> waterFailChance;
 	}
 
 	public static class Armour {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> protectionFailChance;
+		public final ModConfigSpec.ConfigValue<Double> durabilityLossChance;
+		public final ModConfigSpec.ConfigValue<Integer> durabilityDamageMin;
+		public final ModConfigSpec.ConfigValue<Integer> durabilityDamageMax;
 	}
 
 	public static class Totem {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> failChance;
 	}
 
 	public static class Knockback {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> chance;
+		public final ModConfigSpec.ConfigValue<Double> multiplier;
 	}
 
 	public static class DamageReflection {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> chance;
+		public final ModConfigSpec.ConfigValue<Integer> percentageMin;
+		public final ModConfigSpec.ConfigValue<Integer> percentageMax;
+		public final ModConfigSpec.ConfigValue<Boolean> ignoreThorns;
 	}
 
 	public static class Creeper {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> fuseSpeedMultiplier;
+		public final ModConfigSpec.ConfigValue<Double> explosionRadiusMultiplier;
 	}
 
 	public static class Skeleton {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> accuracyMultiplier;
+		public final ModConfigSpec.ConfigValue<Integer> attackSpeedIncrease;
 	}
 
 	public static class Enderman {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> proximityAggroRange;
+		public final ModConfigSpec.ConfigValue<Double> proximityAggroChance;
 	}
 
 	public static class Zombie {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> summonChance;
+		public final ModConfigSpec.ConfigValue<Integer> summonMin;
+		public final ModConfigSpec.ConfigValue<Integer> summonMax;
 	}
 
 	public static class Potion {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> backfireChance;
 	}
 
 	public static class MobRegen {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Integer> delay;
+		public final ModConfigSpec.ConfigValue<Integer> rate;
+		public final ModConfigSpec.ConfigValue<Double> amount;
 	}
 
 	public static class SetFire {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Integer> radius;
+		public final ModConfigSpec.ConfigValue<Double> chance;
+		public final ModConfigSpec.ConfigValue<Integer> initialDuration;
+		public final ModConfigSpec.ConfigValue<Integer> durationIncrease;
 	}
 
 	public static class FoodData {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
+		public final ModConfigSpec.ConfigValue<Double> exhaustionMultiplier;
 	}
 
 	public static class DrunkJumping {
-
+		public final ModConfigSpec.ConfigValue<Boolean> enabled;
 	}
 
 	public static class NightmareEvent {
-
+		public final ModConfigSpec.ConfigValue<Double> chance;
 	}
 
 	static {
