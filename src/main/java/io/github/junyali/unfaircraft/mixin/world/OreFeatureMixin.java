add37cdbe49376407c78a111d0f51df403193884
mochi <git@nyamochi.moe>
2026-04-24 14:22:18 +0100

    Attempt to fix smth causing my datagen to fail???

--- src/main/java/io/github/junyali/unfaircraft/mixin/world/OreFeatureMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/world/OreFeatureMixin.java
@@ -22,31 +22,32 @@ import java.util.Map;
 @Mixin(OreFeature.class)
 public class OreFeatureMixin {
 	@Unique
-	private static final Map<Block, Block> unfaircraft$oreReplacements = new HashMap<>();
+	private static final Block unfaircraft$getReplacement(Block block) {
+		if (block == Blocks.DIAMOND_ORE) return Blocks.COAL_ORE;
+		if (block == Blocks.DEEPSLATE_DIAMOND_ORE) return Blocks.DEEPSLATE_COAL_ORE;
 
-	static {
-		unfaircraft$oreReplacements.put(Blocks.DIAMOND_ORE, Blocks.COAL_ORE);
-		unfaircraft$oreReplacements.put(Blocks.DEEPSLATE_DIAMOND_ORE, Blocks.DEEPSLATE_COAL_ORE);
+		if (block == Blocks.IRON_ORE) return Blocks.COAL_ORE;
+		if (block == Blocks.DEEPSLATE_IRON_ORE) return Blocks.IRON_ORE;
 
-		unfaircraft$oreReplacements.put(Blocks.IRON_ORE, Blocks.COAL_ORE);
-		unfaircraft$oreReplacements.put(Blocks.DEEPSLATE_IRON_ORE, Blocks.DEEPSLATE_COAL_ORE);
+		if (block == Blocks.GOLD_ORE) return Blocks.COAL_ORE;
+		if (block == Blocks.DEEPSLATE_GOLD_ORE) return Blocks.DEEPSLATE_COAL_ORE;
 
-		unfaircraft$oreReplacements.put(Blocks.GOLD_ORE, Blocks.COAL_ORE);
-		unfaircraft$oreReplacements.put(Blocks.DEEPSLATE_GOLD_ORE, Blocks.DEEPSLATE_COAL_ORE);
+		if (block == Blocks.COPPER_ORE) return Blocks.COAL_ORE;
+		if (block == Blocks.DEEPSLATE_COPPER_ORE) return Blocks.DEEPSLATE_COAL_ORE;
 
-		unfaircraft$oreReplacements.put(Blocks.COPPER_ORE, Blocks.COAL_ORE);
-		unfaircraft$oreReplacements.put(Blocks.DEEPSLATE_COPPER_ORE, Blocks.DEEPSLATE_COAL_ORE);
+		if (block == Blocks.REDSTONE_ORE) return Blocks.COAL_ORE;
+		if (block == Blocks.DEEPSLATE_REDSTONE_ORE) return Blocks.DEEPSLATE_COAL_ORE;
 
-		unfaircraft$oreReplacements.put(Blocks.REDSTONE_ORE, Blocks.COAL_ORE);
-		unfaircraft$oreReplacements.put(Blocks.DEEPSLATE_REDSTONE_ORE, Blocks.DEEPSLATE_COAL_ORE);
 
-		unfaircraft$oreReplacements.put(Blocks.LAPIS_ORE, Blocks.COAL_ORE);
-		unfaircraft$oreReplacements.put(Blocks.DEEPSLATE_LAPIS_ORE, Blocks.DEEPSLATE_COAL_ORE);
+		if (block == Blocks.LAPIS_ORE) return Blocks.COAL_ORE;
+		if (block == Blocks.DEEPSLATE_LAPIS_ORE) return Blocks.DEEPSLATE_COAL_ORE;
 
-		unfaircraft$oreReplacements.put(Blocks.EMERALD_ORE, Blocks.MOSS_BLOCK);
-		unfaircraft$oreReplacements.put(Blocks.DEEPSLATE_EMERALD_ORE, Blocks.MOSS_BLOCK);
+		if (block == Blocks.EMERALD_ORE) return Blocks.COAL_ORE;
+		if (block == Blocks.DEEPSLATE_EMERALD_ORE) return Blocks.DEEPSLATE_COAL_ORE;
 
-		unfaircraft$oreReplacements.put(Blocks.ANCIENT_DEBRIS, Blocks.MAGMA_BLOCK);
+		if (block == Blocks.ANCIENT_DEBRIS) return Blocks.MAGMA_BLOCK;
+
+		return null;
 	}
 
 	@Unique
@@ -91,12 +92,12 @@ public class OreFeatureMixin {
 					BlockState blockState = level.getBlockState(blockPos);
 					Block block = blockState.getBlock();
 
-					if (unfaircraft$oreReplacements.containsKey(block)) {
+					Block replacement = unfaircraft$getReplacement(block);
+					if (replacement != null) {
 						float replacementChance = unfaircraft$getReplacementChance(block);
 
 						if (random.nextFloat() < replacementChance) {
-							Block replacementBlock = unfaircraft$oreReplacements.get(block);
-							level.setBlock(blockPos, replacementBlock.defaultBlockState(), 2);
+							level.setBlock(blockPos, replacement.defaultBlockState(), 2);
 						}
 					}
 				}
