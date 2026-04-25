f007c59aa64a6a470de2f616f3861849c6080fbd
mochi <git@nyamochi.moe>
2026-04-25 21:29:30 +0100

    Apply new changes to cave in mixin

--- src/main/java/io/github/junyali/unfaircraft/mixin/block/CaveInMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/block/CaveInMixin.java
@@ -3,24 +3,31 @@ package io.github.junyali.unfaircraft.mixin.block;
 import io.github.junyali.unfaircraft.config.UnfairCraftConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.entity.item.FallingBlockEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Set;
 
-@Mixin(Block.class)
+@Mixin(ServerPlayerGameMode.class)
 public abstract class CaveInMixin {
+	@Shadow
+	protected ServerLevel level;
+
+	@Final
+	@Shadow
+	protected ServerPlayer player;
+
 	@Unique
 	private static final int unfaircraft$scan_height = 16;
 
@@ -42,19 +49,15 @@ public abstract class CaveInMixin {
 	}
 
 	@Inject(
-			method = "playerDestroy",
-			at = @At("TAIL")
+			method = "destroyBlock",
+			at = @At("RETURN")
 	)
-	private void unfaircraft$caveIn(Level level, Player player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack tool, CallbackInfo ci) {
+	private void unfaircraft$caveIn(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+		if (!cir.getReturnValue()) {
+			return;
+		}
+
 		if (!UnfairCraftConfig.isEnabled(UnfairCraftConfig.BLOCK.enabled)) {
-			return;
-		}
-
-		if (level.isClientSide()) {
-			return;
-		}
-
-		if (!(level instanceof ServerLevel serverLevel)) {
 			return;
 		}
 
