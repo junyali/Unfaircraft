a2d8e347b25ed6ac604a8b9c5b0edde76bda189d
mochi <git@nyamochi.moe>
2026-04-28 00:25:56 +0100

    Oops forgot to add glass mixin to mixins.json, made some adjustments

--- src/main/java/io/github/junyali/unfaircraft/mixin/block/GlassMixin.java
+++ src/main/java/io/github/junyali/unfaircraft/mixin/block/GlassMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -19,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Set;
 
-@Mixin(BlockBehaviour.class)
+@Mixin(Block.class)
 public abstract class GlassMixin {
 	@Unique
 	private static Set<Block> unfaircraft$glassBlocks;
@@ -70,10 +69,10 @@ public abstract class GlassMixin {
 	}
 
 	@Inject(
-			method = "entityInside",
+			method = "stepOn",
 			at = @At("HEAD")
 	)
-	private void unfaircraft$glassBreak(BlockState state, Level level, BlockPos pos, Entity entity, CallbackInfo ci) {
+	private void unfaircraft$glassBreak(Level level, BlockPos pos, BlockState state, Entity entity, CallbackInfo ci) {
 		if (unfaircraft$getGlassBlocks().contains(state.getBlock())) {
 			if (!UnfairCraftConfig.isEnabled(UnfairCraftConfig.GLASS.enabled)) {
 				return;
@@ -88,18 +87,20 @@ public abstract class GlassMixin {
 			}
 
 			boolean isSprinting = livingEntity.isSprinting();
-			boolean isLanding = livingEntity.onGround() && livingEntity.getDeltaMovement().y < -0.1;
+			boolean isLanding = livingEntity.fallDistance > 0.5f;
 
 			if (isSprinting || isLanding) {
-				level.destroyBlock(pos, false);
-				level.playSound(
-						null,
-						pos,
-						SoundEvents.GLASS_BREAK,
-						SoundSource.BLOCKS,
-						1.0f,
-						1.0f
-				);
+				if (level.random.nextFloat() < UnfairCraftConfig.GLASS.breakChance.get().floatValue()) {
+					level.destroyBlock(pos, false);
+					level.playSound(
+							null,
+							pos,
+							SoundEvents.GLASS_BREAK,
+							SoundSource.BLOCKS,
+							1.0f,
+							1.0f
+					);
+				}
 			}
 		}
 	}
