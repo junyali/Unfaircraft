d13e62f1ce4b57936e50a7b24e28e847ae4da288
mochi <git@nyamochi.moe>
2026-04-28 21:26:02 +0100

    Setup datagen

---
+++ src/main/java/io/github/junyali/unfaircraft/datagen/DataGenerators.java
@@ -0,0 +1,11 @@
+package io.github.junyali.unfaircraft.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGenerators implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+	}
+}
