d290c4f82921ce18fecb585b52429d8c27748c56
mochi <git@nyamochi.moe>
2026-04-28 03:51:02 +0100

    Enforce mod dependency on both client and server

--- src/main/java/io/github/junyali/unfaircraft/UnfairCraft.java
+++ src/main/java/io/github/junyali/unfaircraft/UnfairCraft.java
@@ -2,15 +2,13 @@ package io.github.junyali.unfaircraft;
 
 import io.github.junyali.unfaircraft.config.UnfairCraftConfig;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(UnfairCraft.MODID)
@@ -19,19 +17,12 @@ public class UnfairCraft {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public UnfairCraft(IEventBus modEventBus, ModContainer modContainer) {
-        modEventBus.addListener(this::commonSetup);
-
-        NeoForge.EVENT_BUS.register(this);
-
         modContainer.registerConfig(ModConfig.Type.COMMON, UnfairCraftConfig.SPEC, "unfaircraft.toml");
+        modEventBus.addListener(this::registerNetworking);
     }
 
-    private void commonSetup(FMLCommonSetupEvent event) {
-        LOGGER.info("HELLO FROM COMMON SETUP");
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("HELLO from server starting");
+    private void registerNetworking(RegisterPayloadHandlersEvent event) {
+        PayloadRegistrar payloadRegistrar = event.registrar(MODID)
+                .versioned("1.1.0");
     }
 }
