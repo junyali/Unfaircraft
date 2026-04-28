6e8a63bdeb4512fd0d7eac8d16a5f77be508dba9
mochi <git@nyamochi.moe>
2026-04-28 18:36:52 +0100

    Rename main class for consistency

--- src/main/java/io/github/junyali/unfaircraft/UnfairCraft.java
+++ src/main/java/io/github/junyali/unfaircraft/UnfairCraft.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Unfaircraft implements ModInitializer {
+public class UnfairCraft implements ModInitializer {
 	public static final String MOD_ID = "unfaircraft";
 
 	// This logger is used to write text to the console and the log file.
