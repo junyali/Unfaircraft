3741341f5a1936efe524647c100c953b566e4cea
mochi <git@nyamochi.moe>
2026-04-28 05:36:53 +0100

    Setup Config interface

---
+++ src/main/java/io/github/junyali/unfaircraft/config/UnfairCraftConfig.java
@@ -0,0 +1,8 @@
+package io.github.junyali.unfaircraft.config;
+
+import io.wispforest.owo.config.annotation.*;
+
+@Modmenu(modId = "unfaircraft")
+@Config(name = "unfaircraft", wrapperName = "UnfaircraftConfig")
+public class UnfairCraftConfig {
+}
